0b505e8b66109e5349f0fa8c60b0a0be0ddcc00d
espsta <espsta@hotmail.com>
2026-02-01 15:12:19 +0100

    Fix supabaseClient to use VITE env vars

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 ﻿import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.https://glcvlzxrbzphafyamynu.supabase.co;
-const supabaseAnonKey = import.meta.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsY3ZsenhyYnpwaGFmeWFteW51Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk5NDc0NjksImV4cCI6MjA4NTUyMzQ2OX0.UKCcE6h5pdwpQJli84f1mlJVm10FhMTw_jdsDj3eVjM;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
